42aae42f09cf34ae593d2fa0c341283743523477
kanayuki <kanayuki@163.com>
2022-11-02 02:20:01 +0800

    Initial commit

---
+++ VGG16/vgg16.py
@@ -0,0 +1,72 @@
+import torch
+from torch import nn
+import numpy as np
+from torchsummary import summary
+import torchvision
+
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+
+        self.module = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.AdaptiveAvgPool2d((7, 7)),
+
+            nn.Flatten(),
+
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 1000),
+        )
+
+    def forward(self, x):
+        x = self.module(x)
+        return x
+
+
+if __name__ == '__main__':
+    # model = VGG16().cuda()
+    # summary(model, (3, 224, 224))
+    #
+    vgg16 = torchvision.models.vgg16().cuda()
+    summary(vgg16, (3, 224, 224))
